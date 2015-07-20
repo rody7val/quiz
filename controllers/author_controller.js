4f7283b9d583a207d29739b64eaf9e610bf3d8dd
Rodolfo Valguarnera <rod7val@gmail.com>
2015-07-19 23:58:51 -0300

    get /author page

--- controllers/author_controller.js
+++ controllers/author_controller.js
@@ -1,8 +1,7 @@
 // GET /quizes/answer
 exports.author = function(req, res){
 	res.render('author', {
-		nombre: 'Rodolfo',
-		apellido: 'Valguarnera',
+		full_name: 'Rodolfo Valguarnera',
 		imagen: '/images/index.jpeg',
 		email: 'rod7val@gmail.com'
 	});
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET /question & /answer page. */
 var quizController = require('../controllers/quiz_controller');
+
+/* GET /author page. */
+var authorController = require('../controllers/author_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -10,5 +14,6 @@ router.get('/', function(req, res) {
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+router.get('/author', authorController.author);
 
 module.exports = router;
