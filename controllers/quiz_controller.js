f68c5989d7c1657791e27d480e83680c2667b4e2
Rodolfo Valguarnera <rod7val@gmail.com>
2015-07-24 20:50:04 -0300

    Multiples preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,19 +1,32 @@
 var models = require('../models/models.js')
 
-// GET /quizes/question
-exports.question = function(req, res){
-	models.Quiz.findAll().success(function(quiz){
-		res.render('quizes/question', {pregunta: quiz[0].pregunta});
+// GET /quizes/:id
+exports.show = function(req, res){
+	models.Quiz.find(req.params.quizId).then(function(quiz){
+		res.render('quizes/show', {quiz: quiz});
 	})
 };
 
 // GET /quizes/answer
 exports.answer = function(req, res){
-	models.Quiz.findAll().success(function(quiz){
-		if (req.query.respuesta === quiz[0].respuesta) {
-			res.render('quizes/answer', {respuesta: 'Correcto'});
+	models.Quiz.find(req.params.quizId).then(function(quiz){
+		if (req.query.respuesta === quiz.respuesta) {
+			res.render('quizes/answer', {
+				respuesta: 'Correcto',
+				quiz: quiz
+			});
 		}else{
-			res.render('quizes/answer', {respuesta: 'Incorrecto'});
+			res.render('quizes/answer', {
+				respuesta: 'Incorrecto',
+				quiz: quiz
+			});
 		}
+	})
+};
+
+// GET /quizes/
+exports.index = function(req, res){
+	models.Quiz.findAll().then(function(quizes){
+		res.render('quizes/index.ejs', {quizes: quizes});
 	})
 };
--- models/models.js
+++ models/models.js
@@ -36,10 +36,17 @@ sequelize.sync().success(function(){
 	//success(..) ejecuta el manejador una vez creada la tabla
 	Quiz.count().success(function(count){
 		if(count === 0) { //la tabla se inicializa solo si esta vacia
-			Quiz.create({ pregunta: 'Capital de Italia', 
-										respuesta: 'Roma'					
-								 })
-			.success(function(){console.log('Base de datos inicializada')})
+			Quiz.create({
+				pregunta: 'Capital de Italia?', 
+				respuesta: 'Roma'					
+			})
+			Quiz.create({
+				pregunta: 'Capital de Portugal?', 
+				respuesta: 'Lisboa'					
+			})
+			.success(function(){
+				console.log('Base de datos inicializada')
+			})
 		}
 	})
 });
