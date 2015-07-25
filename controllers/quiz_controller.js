0ef20c307837113363420294b24cf571f39c758c
Rodolfo Valguarnera <rod7val@gmail.com>
2015-07-25 16:37:11 -0300

    Bucador de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -26,7 +26,16 @@ exports.answer = function(req, res){
 
 // GET /quizes/
 exports.index = function(req, res){
-	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index.ejs', {quizes: quizes});
-	})
+	if(req.query.search){
+		var s = '%' + req.query.search.replace(' ', '%') + '%';
+		models.Quiz.findAll({
+			where: ["pregunta like ?", s]
+		}).then(function(quizes){
+			res.render('quizes/index.ejs', {quizes: quizes});
+		})
+	}else{
+		models.Quiz.findAll().then(function(quizes){
+			res.render('quizes/index.ejs', {quizes: quizes});
+		})
+	}
 };
