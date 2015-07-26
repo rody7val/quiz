754b8832fa60eaae07ee7d4df43fd2ebbd040939
Rodolfo Valguarnera <rod7val@gmail.com>
2015-07-26 16:26:13 -0300

    Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,24 +1,35 @@
 var models = require('../models/models.js')
 
+exports.load = function(req, res, next, quizId){
+	models.Quiz.find(quizId).then(
+		function(quiz){
+			if (quiz){
+				req.quiz = quiz;
+				next();
+			}else{ next(new Error('No existe quizId=' + quizId)) }
+		}
+	).catch(function(error){ next(error); })
+}
+
 // GET /quizes/:id
 exports.show = function(req, res){
 	models.Quiz.find(req.params.quizId).then(function(quiz){
-		res.render('quizes/show', {quiz: quiz});
+		res.render('quizes/show', {quiz: req.quiz});
 	})
 };
 
 // GET /quizes/answer
 exports.answer = function(req, res){
 	models.Quiz.find(req.params.quizId).then(function(quiz){
-		if (req.query.respuesta === quiz.respuesta) {
+		if (req.query.respuesta === req.quiz.respuesta) {
 			res.render('quizes/answer', {
 				respuesta: 'Correcto',
-				quiz: quiz
+				quiz: req.quiz
 			});
 		}else{
 			res.render('quizes/answer', {
 				respuesta: 'Incorrecto',
-				quiz: quiz
+				quiz: req.quiz
 			});
 		}
 	})
@@ -27,15 +38,16 @@ exports.answer = function(req, res){
 // GET /quizes/
 exports.index = function(req, res){
 	if(req.query.search){
-		var s = '%' + req.query.search.replace(' ', '%') + '%';
+		var s = req.query.search
+		// var s = '%' + req.query.search.replace(' ', '%') + '%';
 		models.Quiz.findAll({
-			where: ["pregunta like ?", s]
+			where: ["pregunta like ?", ('%'+s+'%').replace(/\s+/g,'%') ]
 		}).then(function(quizes){
 			res.render('quizes/index.ejs', {quizes: quizes});
-		})
+		}).catch(function(error){ next(error); })
 	}else{
 		models.Quiz.findAll().then(function(quizes){
 			res.render('quizes/index.ejs', {quizes: quizes});
-		})
+		}).catch(function(error){ next(error); })
 	}
 };
--- models/quiz.js
+++ models/quiz.js
@@ -2,8 +2,5 @@ module.exports = function(sequelize, DataTypes) {
 	return sequelize.define('Quiz', {
 		pregunta:  DataTypes.STRING,
 		respuesta: DataTypes.STRING
-	}, {
-  		indexes: [
-  			{ fields: sequelize.fn('lower', sequelize.col('name')) }]
 	})
 }
