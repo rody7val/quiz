9d12ca6a71ec6474151cf6937dfeb89e47a8224c
Rodolfo Valguarnera <rod7val@gmail.com>
2015-08-01 14:59:05 -0300

    change succes x then

--- models/models.js
+++ models/models.js
@@ -32,9 +32,9 @@ var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 exports.Quiz = Quiz; //exportar definicion de tabla Quiz
 
 //sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().success(function(){
-	//success(..) ejecuta el manejador una vez creada la tabla
-	Quiz.count().success(function(count){
+sequelize.sync().then(function(){
+	//then(..) ejecuta el manejador una vez creada la tabla
+	Quiz.count().then(function(count){
 
 		Quiz.destroy({ force: true })
 
@@ -47,7 +47,7 @@ sequelize.sync().success(function(){
 				pregunta: 'Capital de Portugal?', 
 				respuesta: 'Lisboa'					
 			})
-			.success(function(){
+			.then(function(){
 				console.log('Base de datos inicializada')
 			})
 		}
