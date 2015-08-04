4b5ff03385605bc3bd67071f8405af8eead0522e
Rodolfo Valguarnera <rod7val@gmail.com>
2015-08-04 20:26:33 -0300

    crear comentario

--- models/models.js
+++ models/models.js
@@ -26,10 +26,20 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 	}
 );
 
-//Importar la definicion de la tabla Quiz en quiz.js
-var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+//Importar definicion de la tabla Quiz
+var quiz_path = path.join(__dirname, 'quiz');
+var Quiz = sequelize.import(quiz_path);
 
-exports.Quiz = Quiz; //exportar definicion de tabla Quiz
+//Importar definicion de la tabla Comment
+var comment_path = path.join(__dirname, 'comment');
+var Comment = sequelize.import(comment_path);
+
+//Relacion 1 a N
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
+exports.Quiz = Quiz; //exportar tabla Quiz
+exports.Comment = Comment; //exportar tabla Comment
 
 //sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function(){
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET /question & /answer page. */
 var quizController = require('../controllers/quiz_controller');
-
-/* GET /author page. */
+var commentController = require('../controllers/comment_controller');
 var authorController = require('../controllers/author_controller');
 
-/* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors: [] });
 });
@@ -15,6 +12,7 @@ router.get('/', function(req, res) {
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); //autoload :quizId
 
+//Definicion de rutas /quizes
 router.get('/quizes', quizController.index)
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
@@ -24,6 +22,10 @@ router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
 router.put('/quizes/:quizId(\\d+)', quizController.update);
 router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
 
+router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+
+//Definicion de rutas /autor
 router.get('/author', authorController.author);
 
 module.exports = router;
